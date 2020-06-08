d9fe9845a3cd4625c3b15adf8366f14f2a98425c
Daniel <danrocha@gmail.com>
2020-06-08 10:33:37 +0200

    updates

--- create_html.py
+++ create_html.py
@@ -1,25 +1,34 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
+from scrapy_selenium import SeleniumRequest
 
 
-class spider(scrapy.Spider):
+
+class htmlSpider(scrapy.Spider):
     '''Simple scraper to copy a page's HTML, for local development'''
 
     name = "spider"
 
+    def __init__(self, js=False, **kwargs):
+        super().__init__(**kwargs)
+        self.js = js
+
     def start_requests(self):
         urls = [
-            'https://www.wg-gesucht.de/wohnungen-in-Berlin-Kreuzberg.4058192.html']
+            'https://www.immowelt.de/expose/2vekd44']
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            if self.js:
+               yield SeleniumRequest(url=url, callback=self.parse)
+            else:
+                yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         # write out the html
-        html_file = "wohnungen-in-Berlin-Kreuzberg.4058192.html"
+        html_file = "./html/immowelt-expose-2vekd44.html"
         with open(html_file, 'wb') as fout:
             fout.write(response.body)
 
 
 process = CrawlerProcess()
-process.crawl(spider)
+process.crawl(htmlSpider,js=True)
 process.start()
