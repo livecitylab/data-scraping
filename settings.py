aadca60e00cceaeea8a4edd9f16b3b7e6a66b82a
Daniel <danrocha@gmail.com>
2020-06-08 10:34:15 +0200

    add selenium for js pages

--- settings.py
+++ settings.py
@@ -1,5 +1,13 @@
+# from shutil import which
+#
+# SELENIUM_DRIVER_NAME = 'firefox'
+# SELENIUM_DRIVER_EXECUTABLE_PATH = which('geckodriver')
+# SELENIUM_DRIVER_ARGUMENTS=['-headless']
+
 ROTATING_PROXY_LIST_PATH = 'proxies/proxies.txt'
+
 
 DOWNLOADER_MIDDLEWARES = {
     'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
+    # 'scrapy_selenium.SeleniumMiddleware': 800
 }
